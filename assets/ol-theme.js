17dbf56e069003292594977a02ff8d5c275ed57b
ZUNZET <ultimate.rebellion0@gmail.com>
2022-02-25 16:01:26 -0500

    Add filter function

--- assets/ol-theme.js
+++ assets/ol-theme.js
@@ -6,7 +6,8 @@ const OlTheme = (function () {
 
   const setEvents = function () {
     $(document)
-      .on('click', '.js-close-drawer', closeDrawer);
+      .on('click', '.js-close-drawer', closeDrawer)
+      .on('click', '.js-filter-button', customProductFilter);
   }
 
   $(document).ready(function(){
@@ -18,7 +19,21 @@ const OlTheme = (function () {
 
   const closeDrawer = function () {
     document.querySelector('.js-menu-drawer-details').firstElementChild.click();
-  }
+  };
+
+  const customProductFilter = function (event) {
+    const input = event.currentTarget.value;
+    const productArr = Array.from(document.querySelectorAll('.js-featured-product')); 
+
+    productArr.map(x => { 
+      if ( x.getAttribute('product-type') == input || input == 'All' ) {
+        x.removeAttribute('style');        
+      } else {
+        x.style.display = 'none';
+      }
+    });
+
+  };
 
   return {
     init: init
